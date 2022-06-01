db1c9519dad6956968a4bf113a246bec16bff44c
Yarince Pascual-Mateo <ypascual@uncc.edu>
2022-06-01 15:35:43 -0400

    Product.java

--- src/main/java/StoreFront/Product.java
+++ src/main/java/StoreFront/Product.java
@@ -1,5 +1,75 @@
 package StoreFront;
 
-public class Product {
+import java.util.Objects;
 
+public class Product {
+	private String name;
+	private double price;
+	private int id;
+	
+	
+	public Product() {
+		
+	}
+
+
+	public Product(String name, double price, int id) {
+		super();
+		this.name = name;
+		this.price = price;
+		this.id = id;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, price);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return id == other.id && Objects.equals(name, other.name)
+				&& Double.doubleToLongBits(price) == Double.doubleToLongBits(other.price);
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public double getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	
 }
